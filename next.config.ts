e5a805aca36ab869bd0f1065a54fd38877b2b8d2
Seyfullah Tıkıç <stikic@gmail.com>
2025-05-24 22:14:30 +0000

    1

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-        permanent: false,
-      },
-    ];
-  },
+  output: 'export',
 };
 
 export default nextConfig;
