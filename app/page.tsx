3d7481327c1ab84714d1694e491d871a50a77fe7
Seyfullah Tıkıç <stikic@gmail.com>
2025-05-24 21:57:04 +0000

    Committed

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Merhaba Dünya</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <h1>Merhaba Dünya</h1>
-        <p>
-          <Link href="/todo" style={{ fontSize: "1.2rem", color: "#0070f3" }}>
-            Go to Todo Page
-          </Link>
-        </p>
-      </div>
-    </>
+    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <h1>Merhaba Dünya</h1>
+      <p>
+        <Link href="/todo" style={{ fontSize: "1.2rem", color: "#0070f3" }}>
+          Go to Todo Page
+        </Link>
+      </p>
+    </div>
   );
 }
