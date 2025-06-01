777419b2c4d226a1362e2dc95739a9e4ea4b1861
Seyfullah Tıkıç <stikic@gmail.com>
2025-06-01 12:42:19 +0000

    lamda webapi

--- dotnet-core/HelloWorldLambda/Readme.md
+++ dotnet-core/HelloWorldLambda/Readme.md
@@ -1,1 +1,3 @@
+https://nodogmablog.bryanhogan.net/2021/02/c-and-aws-lambdas-part-1-hello-world/
+
 dotnet lambda invoke-function todo-function --cli-binary-format raw-in-base64-out --payload "merhaba dünya"
---
+++ dotnet-core/WebAPILambda/src/WebAPILambda/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPILambda.Models;
+using System;
+
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+
+    [HttpGet]
+    public string Get()
+    {
+        return $"Hello World from inside a Lambda {DateTime.Now}";
+    }
+
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return $"You asked for {id}";
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] Person person)
+    {
+        return Ok($"You sent {person.ToString()}");
+    }
+}
